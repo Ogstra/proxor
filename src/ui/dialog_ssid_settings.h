9bd35a7121afa3562400388fe5d17cfecdf676d1
Franco Beron <111332342+Ogstra@users.noreply.github.com>
2026-03-29 00:06:32 -0300

    feat(ui): add DialogSSIDSettings dialog for on-demand WiFi SSID config
    - New QDialog with enable/disable toggle, SSID list, add/remove controls
    - Loads from and saves to DataStore (ssid_on_demand_enabled, ssid_trigger_list)
    - Prevents duplicate SSIDs; calls DataStore::Save() on accept

---
+++ src/ui/dialog_ssid_settings.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <QDialog>
+
+namespace Ui { class DialogSSIDSettings; }
+
+class DialogSSIDSettings : public QDialog {
+    Q_OBJECT
+public:
+    explicit DialogSSIDSettings(QWidget *parent = nullptr);
+    ~DialogSSIDSettings() override;
+public slots:
+    void accept() override;
+private slots:
+    void on_btn_add_ssid_clicked();
+    void on_btn_remove_ssid_clicked();
+private:
+    Ui::DialogSSIDSettings *ui;
+};
