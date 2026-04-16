a13e045245f13026c45a4696b764a65a1f2c967f
Franco Beron <111332342+Ogstra@users.noreply.github.com>
2026-04-16 18:57:14 -0300

    refactor: declare AvailableThemes and NormalizeTheme in ThemeManager header

--- src/ui/ThemeManager.hpp
+++ src/ui/ThemeManager.hpp
@@ -1,14 +1,20 @@
 #pragma once
 
+#include <QList>
 #include <QObject>
+#include <QPair>
 #include <QString>
 
 class ThemeManager : public QObject {
     Q_OBJECT
 public:
+    using ThemeOption = QPair<QString, QString>;
+
     QString system_style_name = "";
     QString current_theme = "System";
 
+    [[nodiscard]] QList<ThemeOption> AvailableThemes() const;
+    [[nodiscard]] QString NormalizeTheme(const QString &theme) const;
     void ApplyTheme(const QString &theme, bool force = false);
 
 signals:
