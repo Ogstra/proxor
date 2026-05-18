544595300bca9a6352fff1fb52e86cb2ce28ccb8
Franco Beron <111332342+Ogstra@users.noreply.github.com>
2026-05-18 17:26:28 -0300

    fix: strip leading flag from naive username on editor save

--- src/ui/edit/edit_naive.cpp
+++ src/ui/edit/edit_naive.cpp
@@ -2,6 +2,7 @@
 #include "ui_edit_naive.h"
 
 #include "fmt/NaiveBean.hpp"
+#include "main/ProxorGui_Utils.hpp"
 
 #include <QInputDialog>
 
@@ -30,7 +31,7 @@ void EditNaive::onStart(std::shared_ptr<ProxorGui::ProxyEntity> _ent) {
 bool EditNaive::onEnd() {
     auto bean = this->ent->NaiveBean();
 
-    P_SAVE_STRING(username);
+    bean->username = StripLeadingFlag(ui->username->text());
     P_SAVE_STRING(password);
     P_SAVE_COMBO_STRING(protocol);
     P_C_SAVE_STRING(extra_headers);
